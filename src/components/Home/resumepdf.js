e4a605054a1af6a4dc743277cb429a6117ed929f
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2022-10-10 16:45:12 +0530

    Update resumepdf.js

--- src/components/Home/resumepdf.js
+++ src/components/Home/resumepdf.js
@@ -4,8 +4,10 @@ import Resume from "../../images/Resume.pdf";
 const ResumePdf = () => {
   return (
     <div> 
+    
          <object width="100%" height="500" data={Resume} type="application/pdf">
                </object>
+ <a href={Resume} download>Click here to download RESUME!!!</a>
                </div>
   )
 }
