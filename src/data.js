85533f34a83d9ca491a569c4385aca24b38773ae
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2022-10-10 12:43:02 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,32 +1,7 @@
 export const products = [
-    {
+     {
       id: 1,
-      img: "",
-      link: "http://lama.dev",
-    },
-    {
-      id: 2,
-      img: "",
-      link: "http://lama.dev",
-    },
-  //   {
-  //     id: 3,
-  //     img: "",
-  //     link: "http://lama.dev",
-  //   },
-  //   {
-  //     id: 4,
-  //     img: "",
-  //     link: "http://lama.dev",
-  //   },
-  //   {
-  //     id: 5,
-  //     img: "",
-  //     link: "http://lama.dev",
-  //   },
-  //   {
-  //     id: 6,
-  //     img: "",
-  //     link: "http://lama.dev",
-  //   },
+      img: "https://images6.alphacoders.com/330/330109.jpg",
+      link: "https://renturbook.netlify.app/",
+    }
   ];
